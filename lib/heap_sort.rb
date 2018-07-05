87c7a8743c4cf020502563f6d0eeae2fa6bd42a9
jacob barlow <jubby2000@gmail.com>
2018-07-05 12:38:37 -0700

    Resolve heap sort with proc

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -2,32 +2,34 @@ require_relative "heap"
 
 class Array
   def heap_sort!
-    # # prc = Proc.new { |a, b| b <=> a }
-    # partitionEnd = 1
-    # while partitionEnd < self.length 
-    #   BinaryMinHeap.heapify_up(self, partitionEnd, partitionEnd + 1)
-    #   partitionEnd += 1
-    # end
-
-    # partitionEnd = self.length
-    # while partitionEnd > 0
-    #   # debugger
-    #   # self.push(self.shift)
-    #   self[partitionEnd - 1], self[0] = self[0], self[partitionEnd - 1]
-    #   partitionEnd -= 1
-    #   BinaryMinHeap.heapify_down(self, partitionEnd, self.length - 1)
-    # end  
-    # self.unshift(self.pop)
-    # self.reverse!
-
-    2.upto(length).each do |size|
-      BinaryMinHeap.heapify_up(self, size - 1, size)
+    prc = Proc.new { |a, b| a <=> b }
+    partitionEnd = 1
+    while partitionEnd < self.length 
+      BinaryMinHeap.heapify_up(self, partitionEnd, partitionEnd + 1)
+      partitionEnd += 1
     end
-    length.downto(2).each do |size|
-      self[size - 1], self[0] = self[0], self[size - 1]
-      BinaryMinHeap.heapify_down(self, 0, size - 1)
+
+    partitionEnd = self.length
+    while partitionEnd > 0
+      if prc.call(self[0], self[partitionEnd - 1]) == -1 
+        self[partitionEnd - 1], self[0] = self[0], self[partitionEnd - 1]
+      end
+      partitionEnd -= 1
+      BinaryMinHeap.heapify_down(self, 0, partitionEnd - 1)
     end
     self.reverse!
+    # self.unshift(self.pop)
+
+    # 2.upto(length).each do |size|
+    #   BinaryMinHeap.heapify_up(self, size - 1, size)
+    # end
+    # length.downto(2).each do |size|
+    #   p self
+    #   self[size - 1], self[0] = self[0], self[size - 1]
+    #   BinaryMinHeap.heapify_down(self, 0, size - 1)
+    #   p self
+    # end
+    # self.reverse!
   end
 
   
